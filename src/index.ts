76feb69223400b4b60aa0e5ffa0c480f3cb5a31f
Nicholas Kipkoech <nickey968@gmail.com>
2023-11-16 13:18:47 +0300

    made new changes

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,12 @@
 import express from "express";
 import { config } from "dotenv";
+import connectDb from "./config/db";
 
 config();
 const app = express();
+const mongoose_url: any = process.env.MONGO_URL;
+
+connectDb(mongoose_url);
 
 const port = process.env.PORT;
 
