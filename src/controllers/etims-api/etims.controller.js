a54c0448c43bc6d26a611b606d06fddfedaba45a
Nicholas Kipkoech <nickey968@gmail.com>
2023-12-05 13:27:49 +0300

    added different functionalities for the API

--- src/controllers/etims-api/etims.controller.js
+++ src/controllers/etims-api/etims.controller.js
@@ -13,6 +13,7 @@ const initializeDevice = async (req, res) => {
     return res.status(200).json({ response: data });
   } catch (error) {
     console.error(error);
+    return res.status(500).json(error);
   }
 };
 
@@ -28,6 +29,7 @@ const getCodeList = async (req, res) => {
     return res.status(200).json({ response: data });
   } catch (error) {
     console.error(error);
+    return res.status(500).json(error);
   }
 };
 
@@ -38,13 +40,14 @@ const selectCustomerReq = async (req, res) => {
       tin: "A123456789Z",
       bhfId: "00",
       cmckey: "hello",
-      custmTin: custmTin,
+      custmTin,
     };
     const response = await axios.post(`${apiUrl}/selectCustomer`, payload);
     const data = response.data;
     return res.status(200).json({ response: data });
   } catch (error) {
     console.error(error);
+    return res.status(500).json(error);
   }
 };
 
@@ -62,6 +65,352 @@ const noticeSearchReq = async (req, res) => {
     return res.status(200).json({ response: data });
   } catch (error) {
     console.error(error);
+    return res.status(500).json(error);
+  }
+};
+
+const itemClsSearchReq = async (req, res) => {
+  try {
+    const { lastReqDt } = req.body;
+    const payload = {
+      tin: "A123456789Z",
+      bhfId: "00",
+      cmckey: "hello",
+      lastReqDt: lastReqDt,
+    };
+    const response = await axios.post(`${apiUrl}/selectItemClsList`, payload);
+    const data = response.data;
+    return res.status(200).json({ response: data });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json(error);
+  }
+};
+
+const itemSaveReq = async (req, res) => {
+  try {
+    const {
+      itemClsCd,
+      itemCd,
+      itemTyCd,
+      itemNm,
+      itemStdNm,
+      orgnNatCd,
+      pkgUnitCd,
+      qtyUnitCd,
+      taxTyCd,
+      btchNo,
+      bcd,
+      dftPrc,
+      grpPrcL1,
+      grpPrcL2,
+      grpPrcL3,
+      grpPrcL4,
+      grpPrcL5,
+      addInfo,
+      sftyQty,
+      isrcAplcbYn,
+      useYn,
+      regrId,
+      regrNm,
+      modrId,
+      modrNm,
+    } = req.body;
+    const payload = {
+      tin: "A123456789Z",
+      bhfId: "00",
+      cmckey: "hello",
+      itemClsCd,
+      itemCd,
+      itemTyCd,
+      itemNm,
+      itemStdNm,
+      orgnNatCd,
+      pkgUnitCd,
+      qtyUnitCd,
+      taxTyCd,
+      btchNo,
+      bcd,
+      dftPrc,
+      grpPrcL1,
+      grpPrcL2,
+      grpPrcL3,
+      grpPrcL4,
+      grpPrcL5,
+      addInfo,
+      sftyQty,
+      isrcAplcbYn,
+      useYn,
+      regrId,
+      regrNm,
+      modrId,
+      modrNm,
+    };
+    const response = await axios.post(`${apiUrl}/saveItem`, payload);
+    const data = response.data;
+    return res.status(200).json({ response: data });
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+const itemSearchReq = async (req, res) => {
+  try {
+    const { lastReqDt } = req.body;
+    const payload = {
+      tin: "A123456789Z",
+      bhfId: "00",
+      cmckey: "hello",
+      lastReqDt: lastReqDt,
+    };
+    const response = await axios.post(`${apiUrl}/selectItemList`, payload);
+    const data = response.data;
+    return res.status(200).json({ response: data });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json(error);
+  }
+};
+const bhfSearchReq = async (req, res) => {
+  try {
+    const { lastReqDt } = req.body;
+    const payload = {
+      tin: "A123456789Z",
+      bhfId: "00",
+      cmckey: "hello",
+      lastReqDt: lastReqDt,
+    };
+    const response = await axios.post(`${apiUrl}/selectBhfList`, payload);
+    const data = response.data;
+    return res.status(200).json({ response: data });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json(error);
+  }
+};
+
+const bhfCustSaveReq = async (req, res) => {
+  try {
+    const {
+      custNo,
+      custTin,
+      custNm,
+      adrs,
+      telNo,
+      email,
+      faxNo,
+      useYn,
+      remark,
+      regrId,
+      regrNm,
+      modrId,
+      modrNm,
+    } = req.body;
+    const payload = {
+      tin: "A123456789Z",
+      bhfId: "00",
+      cmckey: "hello",
+      custNo,
+      custTin,
+      custNm,
+      adrs,
+      telNo,
+      email,
+      faxNo,
+      useYn,
+      remark,
+      regrId,
+      regrNm,
+      modrId,
+      modrNm,
+    };
+    const response = await axios.post(`${apiUrl}/saveBhfCustomer`, payload);
+    const data = response.data;
+    return res.status(200).json({ response: data });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json(error);
+  }
+};
+
+const bhfUserSaveReq = async (req, res) => {
+  try {
+    const {
+      userId,
+      userNm,
+      pwd,
+      adrs,
+      cntc,
+      authCd,
+      useYn,
+      remark,
+      regrId,
+      regrNm,
+      modrId,
+      modrNm,
+    } = req.body;
+    const payload = {
+      tin: "A123456789Z",
+      bhfId: "00",
+      cmckey: "hello",
+      userId,
+      userNm,
+      pwd,
+      adrs,
+      cntc,
+      authCd,
+      useYn,
+      remark,
+      regrId,
+      regrNm,
+      modrId,
+      modrNm,
+    };
+    const response = await axios.post(`${apiUrl}/saveBhfUser`, payload);
+    const data = response.data;
+    return res.status(200).json({ response: data });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json(error);
+  }
+};
+
+const bhfInsuranceSaveReq = async (req, res) => {
+  try {
+    const { isrccCd, isrccNm, isrcRt, useYn, regrId, regrNm, modrId, modrNm } =
+      req.body;
+    const payload = {
+      tin: "A123456789Z",
+      bhfId: "00",
+      cmckey: "hello",
+      isrcRt,
+      useYn,
+      isrccCd,
+      isrccNm,
+      regrId,
+      regrNm,
+      modrId,
+      modrNm,
+    };
+    const response = await axios.post(`${apiUrl}/saveBhfInsurance`, payload);
+    const data = response.data;
+    return res.status(200).json({ response: data });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json(error);
+  }
+};
+const importItemSearchReq = async (req, res) => {
+  try {
+    const { lastReqDt } = req.body;
+    const payload = {
+      tin: "A123456789Z",
+      bhfId: "00",
+      cmckey: "hello",
+      lastReqDt: lastReqDt,
+    };
+    const response = await axios.post(
+      `${apiUrl}/selectImportItemList`,
+      payload
+    );
+    const data = response.data;
+    return res.status(200).json({ response: data });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json(error);
+  }
+};
+const importItemUpdateReq = async (req, res) => {
+  try {
+    const {
+      taskCd,
+      dclDe,
+      itemSeq,
+      hsCd,
+      itemClCd,
+      itemCd,
+      imptItemsttsCd,
+      remark,
+      modrId,
+      modrNm,
+    } = req.body;
+    const payload = {
+      tin: "A123456789Z",
+      bhfId: "00",
+      cmckey: "hello",
+      taskCd,
+      dclDe,
+      itemSeq,
+      hsCd,
+      itemClCd,
+      itemCd,
+      imptItemsttsCd,
+      remark,
+      modrId,
+      modrNm,
+    };
+    const response = await axios.post(`${apiUrl}/updateImportItem`, payload);
+    const data = response.data;
+    return res.status(200).json({ response: data });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json(error);
+  }
+};
+const trnsSalesSaveWrReq = async (req, res) => {
+  try {
+    const requestData = req.body;
+    const payload = {
+      tin: "A123456789Z",
+      bhfId: "00",
+      cmckey: "hello",
+      requestData,
+    };
+    const response = await axios.post(`${apiUrl}/saveTrnsSalesOsdc`, payload);
+    const data = response.data;
+    return res.status(200).json({ response: data });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json(error);
+  }
+};
+
+const trnsPurchaseSalesReq = async (req, res) => {
+  try {
+    const requestData = req.body;
+    const payload = {
+      tin: "A123456789Z",
+      bhfId: "00",
+      cmckey: "hello",
+      requestData,
+    };
+    const response = await axios.post(
+      `${apiUrl}/selectTrnsPurchaseSalesList`,
+      payload
+    );
+    const data = response.data;
+    return res.status(200).json({ response: data });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json(error);
+  }
+};
+
+const trnsPurchaseSaveReq = async (req, res) => {
+  try {
+    const requestData = req.body;
+    const payload = {
+      tin: "A123456789Z",
+      bhfId: "00",
+      cmckey: "hello",
+      requestData,
+    };
+    const response = await axios.post(`${apiUrl}/insertTrnsPurchase`, payload);
+    const data = response.data;
+    return res.status(200).json({ response: data });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json(error);
   }
 };
 
@@ -70,4 +419,16 @@ export default {
   getCodeList,
   selectCustomerReq,
   noticeSearchReq,
+  itemClsSearchReq,
+  itemSaveReq,
+  itemSearchReq,
+  bhfSearchReq,
+  bhfCustSaveReq,
+  bhfUserSaveReq,
+  bhfInsuranceSaveReq,
+  importItemSearchReq,
+  importItemUpdateReq,
+  trnsSalesSaveWrReq,
+  trnsPurchaseSalesReq,
+  trnsPurchaseSaveReq,
 };
--- src/routes/etims/etims.route.js
+++ src/routes/etims/etims.route.js
@@ -6,5 +6,23 @@ etimsAPIRouter.post("/initialize", etimsController.initializeDevice);
 etimsAPIRouter.post("/selectCodeList", etimsController.getCodeList);
 etimsAPIRouter.post("/selectCustomer", etimsController.selectCustomerReq);
 etimsAPIRouter.post("/selectNoticeList", etimsController.noticeSearchReq);
+etimsAPIRouter.post("/selectItemClsList", etimsController.itemClsSearchReq);
+etimsAPIRouter.post("/saveItem", etimsController.itemSaveReq);
+etimsAPIRouter.post("/selectItemList", etimsController.itemSearchReq);
+etimsAPIRouter.post("/selectBhfList", etimsController.bhfSearchReq);
+etimsAPIRouter.post("/saveBhfCustomer", etimsController.bhfCustSaveReq);
+etimsAPIRouter.post("/saveBhfUser", etimsController.bhfUserSaveReq);
+etimsAPIRouter.post("/saveBhfInsurance", etimsController.bhfInsuranceSaveReq);
+etimsAPIRouter.post(
+  "/selectImportItemList",
+  etimsController.importItemSearchReq
+);
+etimsAPIRouter.post("/updateImportItem", etimsController.importItemUpdateReq);
+etimsAPIRouter.post("/saveTrnsSalesOsdc", etimsController.trnsSalesSaveWrReq);
+etimsAPIRouter.post(
+  "/selectTrnsPurchaseSalesList",
+  etimsController.trnsPurchaseSalesReq
+);
+etimsAPIRouter.post("/insertTrnsPurchase", etimsController.trnsPurchaseSaveReq);
 
 export default etimsAPIRouter;
