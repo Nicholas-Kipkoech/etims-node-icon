f571ae5ba1db2c95a988396327d355e7fd6e9fe8
Nicholas Kipkoech <nickey968@gmail.com>
2023-12-06 16:07:41 +0300

    refactor code

---
+++ src/controllers/etims-api/etims-controller.js
@@ -0,0 +1,521 @@
+import axios from "axios";
+import { config } from "dotenv";
+import { generateRandom8DigitNumber } from "../../utils/helpers.js";
+
+config();
+
+class EtimsController {
+  constructor() {
+    this.apiUrl = process.env.ETIMS_URL;
+    this.defaultHeaders = {
+      cmcKey: process.env.CMCKEY,
+      tin: process.env.TIN,
+      bhfId: process.env.BHFID,
+    };
+  }
+  async makeApiRequest(endpoint, requestData) {
+    try {
+      const payload = {
+        ...requestData,
+      };
+
+      const response = await axios.post(`${this.apiUrl}/${endpoint}`, payload, {
+        headers: this.defaultHeaders,
+      });
+      return response.data;
+    } catch (error) {
+      console.error(error);
+      throw error;
+    }
+  }
+  async initializeDevice(req, res) {
+    try {
+      const { dvcSrlNo } = req.body;
+      const data = await this.makeApiRequest("selectInitOsdcInfo", {
+        dvcSrlNo: dvcSrlNo,
+      });
+      return res.status(200).json({ response: data });
+    } catch (error) {
+      return res.status(500).json(error);
+    }
+  }
+
+  async getCodeList(req, res) {
+    try {
+      const { lastReqDt } = req.body;
+      const data = await this.makeApiRequest("selectCodeList", {
+        lastReqDt: lastReqDt,
+      });
+      return res.status(200).json({ response: data });
+    } catch (error) {
+      console.error(error);
+      return res.status(500).json(error);
+    }
+  }
+
+  async selectCustomerReq(req, res) {
+    try {
+      const { custmTin } = req.body;
+
+      const data = await this.makeApiRequest("selectCustomer", {
+        custmTin: custmTin,
+      });
+      return res.status(200).json({ response: data });
+    } catch (error) {
+      console.error(error);
+      return res.status(500).json(error);
+    }
+  }
+  async noticeSearchReq(req, res) {
+    try {
+      const { lastReqDt } = req.body;
+      const data = await this.makeApiRequest("selectNoticeList", {
+        lastReqDt: lastReqDt,
+      });
+      return res.status(200).json({ response: data });
+    } catch (error) {
+      console.error(error);
+      return res.status(500).json(error);
+    }
+  }
+  async itemClsSearchReq(req, res) {
+    try {
+      const { lastReqDt } = req.body;
+
+      const data = await this.makeApiRequest("selectItemClsList", {
+        lastReqDt: lastReqDt,
+      });
+      return res.status(200).json({ response: data });
+    } catch (error) {
+      console.error(error);
+      return res.status(500).json(error);
+    }
+  }
+  async itemSaveReq(req, res) {
+    try {
+      const {
+        itemClsCd,
+        itemCd,
+        itemTyCd,
+        itemNm,
+        itemStdNm,
+        orgnNatCd,
+        pkgUnitCd,
+        qtyUnitCd,
+        taxTyCd,
+        btchNo,
+        bcd,
+        dftPrc,
+        grpPrcL1,
+        grpPrcL2,
+        grpPrcL3,
+        grpPrcL4,
+        grpPrcL5,
+        addInfo,
+        sftyQty,
+        isrcAplcbYn,
+        useYn,
+        regrId,
+        regrNm,
+        modrId,
+        modrNm,
+      } = req.body;
+      const payload = {
+        itemClsCd,
+        itemCd,
+        itemTyCd,
+        itemNm,
+        itemStdNm,
+        orgnNatCd,
+        pkgUnitCd,
+        qtyUnitCd,
+        taxTyCd,
+        btchNo,
+        bcd,
+        dftPrc,
+        grpPrcL1,
+        grpPrcL2,
+        grpPrcL3,
+        grpPrcL4,
+        grpPrcL5,
+        addInfo,
+        sftyQty,
+        isrcAplcbYn,
+        useYn,
+        regrId,
+        regrNm,
+        modrId,
+        modrNm,
+      };
+      const data = await this.makeApiRequest("saveItem", payload);
+      return res.status(200).json({ response: data });
+    } catch (error) {
+      console.error(error);
+      return res.status(500).json(error);
+    }
+  }
+  async itemSearchReq(req, res) {
+    try {
+      const { lastReqDt } = req.body;
+      const data = await this.makeApiRequest("selectItemList", {
+        lastReqDt,
+      });
+      return res.status(200).json({ response: data });
+    } catch (error) {
+      console.error(error);
+      return res.status(500).json(error);
+    }
+  }
+
+  async bhfSearchReq(req, res) {
+    try {
+      const { lastReqDt } = req.body;
+      const data = await this.makeApiRequest("selectBhfList", {
+        lastReqDt,
+      });
+      return res.status(200).json({ response: data });
+    } catch (error) {
+      console.error(error);
+      return res.status(500).json(error);
+    }
+  }
+
+  async bhfCustSaveReq(req, res) {
+    try {
+      const {
+        custNo,
+        custTin,
+        custNm,
+        adrs,
+        telNo,
+        email,
+        faxNo,
+        useYn,
+        remark,
+        regrId,
+        regrNm,
+        modrId,
+        modrNm,
+      } = req.body;
+      const payload = {
+        custNo,
+        custTin,
+        custNm,
+        adrs,
+        telNo,
+        email,
+        faxNo,
+        useYn,
+        remark,
+        regrId,
+        regrNm,
+        modrId,
+        modrNm,
+      };
+      const data = await this.makeApiRequest("saveBhfCustomer", payload);
+      return res.status(200).json({ response: data });
+    } catch (error) {
+      console.error(error);
+      return res.status(500).json(error);
+    }
+  }
+  async bhfUserSaveReq(req, res) {
+    try {
+      const {
+        userId,
+        userNm,
+        pwd,
+        adrs,
+        cntc,
+        authCd,
+        useYn,
+        remark,
+        regrId,
+        regrNm,
+        modrId,
+        modrNm,
+      } = req.body;
+      const payload = {
+        userId,
+        userNm,
+        pwd,
+        adrs,
+        cntc,
+        authCd,
+        useYn,
+        remark,
+        regrId,
+        regrNm,
+        modrId,
+        modrNm,
+      };
+      const data = await this.makeApiRequest("saveBhfUser", payload);
+      return res.status(200).json({ response: data });
+    } catch (error) {
+      console.error(error);
+      return res.status(500).json(error);
+    }
+  }
+  async bhfInsuranceSaveReq(req, res) {
+    try {
+      const {
+        isrccCd,
+        isrccNm,
+        isrcRt,
+        useYn,
+        regrId,
+        regrNm,
+        modrId,
+        modrNm,
+      } = req.body;
+      const payload = {
+        isrcRt,
+        useYn,
+        isrccCd,
+        isrccNm,
+        regrId,
+        regrNm,
+        modrId,
+        modrNm,
+      };
+      const data = await this.makeApiRequest("saveBhfInsurance", payload);
+      return res.status(200).json({ response: data });
+    } catch (error) {
+      console.error(error);
+      return res.status(500).json(error);
+    }
+  }
+  async importItemSearchReq(req, res) {
+    try {
+      const { lastReqDt } = req.body;
+
+      const data = await this.makeApiRequest("selectImportItemList", {
+        lastReqDt,
+      });
+      return res.status(200).json({ response: data });
+    } catch (error) {
+      console.error(error);
+      return res.status(500).json(error);
+    }
+  }
+  async importItemUpdateReq(req, res) {
+    try {
+      const data = await this.makeApiRequest("updateImportItem", req.body);
+      return res.status(200).json({ response: data });
+    } catch (error) {
+      console.error(error);
+      return res.status(500).json(error);
+    }
+  }
+  async trnsSalesSaveWrReq(req, res) {
+    try {
+      const {
+        trdInvcNo,
+        orgInvcNo,
+        custTin,
+        custNm,
+        salesTyCd,
+        rcptTyCd,
+        pmtTyCd,
+        salesSttsCd,
+        cfmDt,
+        salesDt,
+        stockRlsDt,
+        cnclReqDt,
+        cnclDt,
+        rfdDt,
+        rfdRsnCd,
+        totItemCnt,
+        taxblAmtA,
+        taxblAmtB,
+        taxblAmtC,
+        taxblAmtD,
+        taxblAmtE,
+        taxRtA,
+        taxRtB,
+        taxRtC,
+        taxRtD,
+        taxRtE,
+        taxAmtA,
+        taxAmtB,
+        taxAmtC,
+        taxAmtD,
+        taxAmtE,
+        totTaxblAmt,
+        totTaxAmt,
+        totAmt,
+        prchrAcptcYn,
+        remark,
+        regrId,
+        regrNm,
+        modrId,
+        modrNm,
+        receipt,
+        itemList,
+      } = req.body;
+      const invcNo = generateRandom8DigitNumber();
+      const payload = {
+        trdInvcNo,
+        invcNo,
+        orgInvcNo,
+        custTin,
+        custNm,
+        salesTyCd,
+        rcptTyCd,
+        pmtTyCd,
+        salesSttsCd,
+        cfmDt,
+        salesDt,
+        stockRlsDt,
+        cnclReqDt,
+        cnclDt,
+        rfdDt,
+        rfdRsnCd,
+        totItemCnt,
+        taxblAmtA,
+        taxblAmtB,
+        taxblAmtC,
+        taxblAmtD,
+        taxblAmtE,
+        taxRtA,
+        taxRtB,
+        taxRtC,
+        taxRtD,
+        taxRtE,
+        taxAmtA,
+        taxAmtB,
+        taxAmtC,
+        taxAmtD,
+        taxAmtE,
+        totTaxblAmt,
+        totTaxAmt,
+        totAmt,
+        prchrAcptcYn,
+        remark,
+        regrId,
+        regrNm,
+        modrId,
+        modrNm,
+        receipt,
+        itemList,
+      };
+      const data = await this.makeApiRequest("saveTrnsSalesOsdc", payload);
+      return res.status(200).json({ response: data });
+    } catch (error) {
+      console.error(error);
+      return res.status(500).json(error);
+    }
+  }
+  async trnsPurchaseSalesReq(req, res) {
+    try {
+      const { lastReqDt } = req.body;
+      const data = await this.makeApiRequest("selectTrnsPurchaseSalesList", {
+        lastReqDt,
+      });
+      return res.status(200).json({ response: data });
+    } catch (error) {
+      console.error(error);
+      return res.status(500).json(error);
+    }
+  }
+  async trnsPurchaseSaveReq(req, res) {
+    try {
+      const {
+        orgInvcNo,
+        spplrTin,
+        spplrBhfId,
+        spplrNm,
+        spplrInvcNo,
+        regTyCd,
+        pchsTyCd,
+        rcptTyCd,
+        pmtTyCd,
+        pchsSttsCd,
+        cfmDt,
+        pchsDt,
+        wrhsDt,
+        cnclReqDt,
+        cnclDt,
+        rfdDt,
+        totItemCnt,
+        taxblAmtA,
+        taxblAmtB,
+        taxblAmtC,
+        taxblAmtD,
+        taxblAmtE,
+        taxRtA,
+        taxRtB,
+        taxRtC,
+        taxRtD,
+        taxRtE,
+        taxAmtA,
+        taxAmtB,
+        taxAmtC,
+        taxAmtD,
+        taxAmtE,
+        totTaxblAmt,
+        totTaxAmt,
+        totAmt,
+        remark,
+        regrId,
+        regrNm,
+        modrId,
+        modrNm,
+        itemList,
+      } = req.body;
+      const invcNo = generateRandom8DigitNumber();
+      const payload = {
+        orgInvcNo,
+        invcNo,
+        spplrTin,
+        spplrBhfId,
+        spplrNm,
+        spplrInvcNo,
+        regTyCd,
+        pchsTyCd,
+        rcptTyCd,
+        pmtTyCd,
+        pchsSttsCd,
+        cfmDt,
+        pchsDt,
+        wrhsDt,
+        cnclReqDt,
+        cnclDt,
+        rfdDt,
+        totItemCnt,
+        taxblAmtA,
+        taxblAmtB,
+        taxblAmtC,
+        taxblAmtD,
+        taxblAmtE,
+        taxRtA,
+        taxRtB,
+        taxRtC,
+        taxRtD,
+        taxRtE,
+        taxAmtA,
+        taxAmtB,
+        taxAmtC,
+        taxAmtD,
+        taxAmtE,
+        totTaxblAmt,
+        totTaxAmt,
+        totAmt,
+        remark,
+        regrId,
+        regrNm,
+        modrId,
+        modrNm,
+        itemList,
+      };
+      const data = await this.makeApiRequest("insertTrnsPurchase", payload);
+      return res.status(200).json({ response: data });
+    } catch (error) {
+      console.error(error);
+      return res.status(500).json(error);
+    }
+  }
+}
+
+const etimsController = new EtimsController();
+export default etimsController;
--- src/routes/etims/etims.route.js
+++ src/routes/etims/etims.route.js
@@ -1,28 +1,54 @@
 import { Router } from "express";
-import etimsController from "../../controllers/etims-api/etims.controller.js";
+import etimsController from "../../controllers/etims-api/etims-controller.js";
 const etimsAPIRouter = Router();
 
-etimsAPIRouter.post("/initialize", etimsController.initializeDevice);
-etimsAPIRouter.post("/selectCodeList", etimsController.getCodeList);
-etimsAPIRouter.post("/selectCustomer", etimsController.selectCustomerReq);
-etimsAPIRouter.post("/selectNoticeList", etimsController.noticeSearchReq);
-etimsAPIRouter.post("/selectItemClsList", etimsController.itemClsSearchReq);
-etimsAPIRouter.post("/saveItem", etimsController.itemSaveReq);
-etimsAPIRouter.post("/selectItemList", etimsController.itemSearchReq);
-etimsAPIRouter.post("/selectBhfList", etimsController.bhfSearchReq);
-etimsAPIRouter.post("/saveBhfCustomer", etimsController.bhfCustSaveReq);
-etimsAPIRouter.post("/saveBhfUser", etimsController.bhfUserSaveReq);
-etimsAPIRouter.post("/saveBhfInsurance", etimsController.bhfInsuranceSaveReq);
-etimsAPIRouter.post(
-  "/selectImportItemList",
-  etimsController.importItemSearchReq
-);
-etimsAPIRouter.post("/updateImportItem", etimsController.importItemUpdateReq);
-etimsAPIRouter.post("/saveTrnsSalesOsdc", etimsController.trnsSalesSaveWrReq);
-etimsAPIRouter.post(
-  "/selectTrnsPurchaseSalesList",
-  etimsController.trnsPurchaseSalesReq
-);
-etimsAPIRouter.post("/insertTrnsPurchase", etimsController.trnsPurchaseSaveReq);
+etimsAPIRouter.post("/initialize", (req, res) => {
+  etimsController.initializeDevice(req, res);
+});
+etimsAPIRouter.post("/selectCodeList", (req, res) => {
+  etimsController.getCodeList(req, res);
+});
+etimsAPIRouter.post("/selectCustomer", (req, res) => {
+  etimsController.selectCustomerReq(req, res);
+});
+etimsAPIRouter.post("/selectNoticeList", (req, res) => {
+  etimsController.noticeSearchReq(req, res);
+});
+etimsAPIRouter.post("/selectItemClsList", (req, res) => {
+  etimsController.itemClsSearchReq(req, res);
+});
+etimsAPIRouter.post("/saveItem", (req, res) => {
+  etimsController.itemSaveReq(req, res);
+});
+etimsAPIRouter.post("/selectItemList", (req, res) => {
+  etimsController.itemSearchReq(req, res);
+});
+etimsAPIRouter.post("/selectBhfList", (req, res) => {
+  etimsController.bhfSearchReq(req, res);
+});
+etimsAPIRouter.post("/saveBhfCustomer", (req, res) => {
+  etimsController.bhfCustSaveReq(req, res);
+});
+etimsAPIRouter.post("/saveBhfUser", (req, res) => {
+  etimsController.bhfUserSaveReq(req, res);
+});
+etimsAPIRouter.post("/saveBhfInsurance", (req, res) => {
+  etimsController.bhfInsuranceSaveReq(req, res);
+});
+etimsAPIRouter.post("/selectImportItemList", (req, res) => {
+  etimsController.importItemSearchReq(req, res);
+});
+etimsAPIRouter.post("/updateImportItem", (req, res) => {
+  etimsController.importItemUpdateReq(req, res);
+});
+etimsAPIRouter.post("/saveTrnsSalesOsdc", (req, res) => {
+  etimsController.trnsSalesSaveWrReq(req, res);
+});
+etimsAPIRouter.post("/selectTrnsPurchaseSalesList", (req, res) => {
+  etimsController.trnsPurchaseSalesReq(req, res);
+});
+etimsAPIRouter.post("/insertTrnsPurchase", (req, res) => {
+  etimsController.trnsPurchaseSaveReq(req, res);
+});
 
 export default etimsAPIRouter;
