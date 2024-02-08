f07bc19dde5ee65f20a31132f333e88c06c09eb0
Nicholas Kipkoech <nickey968@gmail.com>
2024-02-08 12:04:58 +0300

    update

--- src/index.js
+++ src/index.js
@@ -16,7 +16,7 @@ app.use(express.json());
 
 app.use(
   cors({
-    origin: "http://localhost:3000", // Allow requests from this origin
+    origin: "*", // Allow requests from this origin
   })
 );
 const port = process.env.PORT;
