0b27cf0697d96300ae7aeb3d42bcf80de19d2629
Nicholas Kipkoech <nickey968@gmail.com>
2024-02-08 11:57:27 +0300

    update some changes

--- src/index.js
+++ src/index.js
@@ -13,7 +13,12 @@ connectToDb();
 const app = express();
 
 app.use(express.json());
-app.use(cors());
+
+app.use(
+  cors({
+    origin: "http://localhost:3000", // Allow requests from this origin
+  })
+);
 const port = process.env.PORT;
 
 const server = app.listen(port, () =>
