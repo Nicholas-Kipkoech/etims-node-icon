b71b2301d10da615ed7c67bfb233707fa513cca9
Nicholas Kipkoech <nickey968@gmail.com>
2024-02-27 14:01:00 +0300

    refactor the organization code

--- src/controllers/organization.controller.js
+++ src/controllers/organization.controller.js
@@ -1,52 +1,16 @@
-import OrganizationDTO from "../databases/organizations.js";
-
-import {
-  generateOrganizationAPIKey,
-  generateRandom8DigitNumber,
-} from "../utils/helpers.js";
-import passHash from "../utils/passHash.js";
-import { sendEmail } from "../utils/sendEmail.js";
+import Organization from "../databases/organizations";
 
 class OrganizationController {
   constructor() {}
   async createOrganization(req, res) {
     try {
-      const { organization_email, organization_name, organization_phone } =
-        req.body;
-      const { error } = validateOrg(req.body);
-      if (error) {
-        return res.status(400).json({ error: error.message });
-      }
+      const { organization_name, organization_type } = req.body;
 
-      const checkOrganization = await OrganizationDTO.Organization.findOne({
-        organization_email: organization_email,
-      });
-      if (checkOrganization) {
-        return res.status(400).json({ error: "Organization already exists!!" });
-      }
-      const random8digit = generateRandom8DigitNumber().toString();
-      const genPassword = await passHash.encrypt(random8digit);
-      const new_org = new OrganizationDTO.Organization({
-        organization_email,
+      const new_org = new Organization({
         organization_name,
-        organization_phone,
-      });
-
-      const generatedKey = generateOrganizationAPIKey();
-      const newKey = new OrganizationDTO.APICredentials({
-        organization: new_org._id,
-        clientKey: generatedKey,
+        organization_type,
         created_at: Date.now(),
       });
-      await newKey.save();
-      sendEmail(
-        organization_email,
-        "Account Creation",
-        random8digit,
-        organization_name,
-        generatedKey
-      );
-
       await new_org.save();
       return res.status(200).json({ organization: new_org });
     } catch (error) {
@@ -57,9 +21,7 @@ class OrganizationController {
 
   async fetchOrganizations(req, res) {
     try {
-      const registered_organizations = await OrganizationDTO.Organization.find(
-        {}
-      );
+      const registered_organizations = await Organization.find({});
       if (registered_organizations) {
         return res.status(200).json({ registered_organizations });
       }
--- src/databases/organizations.js
+++ src/databases/organizations.js
@@ -2,30 +2,10 @@ import mongoose from "mongoose";
 
 const organizationSchema = new mongoose.Schema({
   organization_name: { type: String, required: true },
-  organization_email: { type: String, required: true },
-  organization_phone: { type: String, required: true },
+  organization_type: { type: String, required: true },
   created_at: { type: Date, default: Date.now() },
-});
-
-const organizationAPICredentials = new mongoose.Schema({
-  organization: { type: mongoose.Types.ObjectId, ref: "Organization" },
-  clientKey: { type: String, unique: true },
-  created_at: { type: Date, default: Date.now() },
-});
-
-const etimsCredentials = new mongoose.Schema({
-  organizationId: { type: String },
-  pin: { type: String },
-  branchId: { type: String },
-  taxpayerName: { type: String },
-  cmcKey: { type: String },
-  createdAt: { type: Date, default: Date.now() },
 });
 
 const Organization = mongoose.model("Organization", organizationSchema);
-const APICredentials = mongoose.model(
-  "APICredentials",
-  organizationAPICredentials
-);
-const ETIMSCredentials = mongoose.model("ETIMSCredentials", etimsCredentials);
-export default { Organization, APICredentials, ETIMSCredentials };
+
+export default Organization;
